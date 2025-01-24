c99cea633406f1832cc33ccf2fd74d8047d5a94b
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-01-24 10:14:56 +0700

    Saving and Extending conversations and summarizations Conversation Summary Memory

--- chat/main.py
+++ chat/main.py
@@ -1,15 +1,20 @@
 from langchain.chat_models import ChatOpenAI
 from langchain import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)
 # gửi thông điệp humanMessage bằng conversationBufferMemory đi
 
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages", 
+    return_messages=True,
+    llm=chat
+    )
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
     messages=[
@@ -22,7 +27,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat,
     prompt=prompt,
-    memory=memory
+    memory=memory,
+    verbose=True  # bật debug xem coi human said and chain rep;u
 )
 
 while True:
