73aa8d60586b759e69f6fb560cf9cf9647bf9dc6
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-01-24 13:47:14 +0700

    Tách chunk

--- facts/main.py
+++ facts/main.py
@@ -1,10 +1,23 @@
 from langchain.document_loaders import TextLoader
+# dùng thư viện để chunk text trong file
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# làm cách nào để upload file
+
+text_splitter = CharacterTextSplitter(
+  separator="\n",
+  chunk_size=200,
+  chunk_overlap=0
+)
+# làm cách nào để upload file -- sử dụng text loader
 loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+  text_splitter=text_splitter
+)
+
+
 
 # load file trong txt ra
 docs = loader.load()
