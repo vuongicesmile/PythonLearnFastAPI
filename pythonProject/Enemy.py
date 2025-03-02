26015699794759fb4da9eb6c9cfbfaa128cbb700
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 09:03:23 +0700

    OOP-python

---
+++ pythonProject/Enemy.py
@@ -0,0 +1,7 @@
+class Enemy:
+
+    type_of_enemy: str
+    health_points: int = 10
+    attach_damage: int = 1
+
+    print(health_points)
---
+++ pythonProject/Main.py
@@ -0,0 +1,9 @@
+from Enemy import *
+
+
+enemy = Enemy()
+
+enemy.type_of_enemy = 'Zombie'
+
+print(f'{enemy.type_of_enemy} has {enemy.health_points} health points'
+      f' and can do an attach ')
