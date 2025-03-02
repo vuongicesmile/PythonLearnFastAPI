08f4cc08aecfa2aff176acf1f91b7958df10f978
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 17:16:06 +0700

    Get - dung path parameter de filter book

--- fastAPI/books.py
+++ fastAPI/books.py
@@ -23,3 +23,10 @@ async  def first_api():
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        # casrfold = lowercase
+        if book.get('title').casefold() == book_title.casefold():
+            return book
