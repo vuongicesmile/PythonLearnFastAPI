14f19f28edd8e18c245b3246c812448ccd384a36
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 17:36:50 +0700

    POST - create book

--- fastAPI/books.py
+++ fastAPI/books.py
@@ -49,3 +49,19 @@ async def read_books_by_author_path(author: str):
             books_to_return.append(book)
 
     return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        # dung dau \ de xuong hang khi code
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
