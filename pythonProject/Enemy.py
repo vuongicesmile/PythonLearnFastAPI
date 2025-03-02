773573a24b63d096bfb1d2f3d82767ec8a434fbc
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 10:32:14 +0700

    Encapsulation- tinh dong goi - chuyen thuoc tinh pubic => priave , dung getter de get private value

--- pythonProject/Enemy.py
+++ pythonProject/Enemy.py
@@ -7,16 +7,22 @@ class Enemy:
     # python tu tao constuctor cho ban
     def __init__(self, type_of_enemy, health_points, attach_damage):
         # print('create an enamy')
-        self.type_of_enemy = type_of_enemy
+        self.__type_of_enemy = type_of_enemy # change private to use double undersource
         self.health_points = health_points
         self.attach_damage = attach_damage
 
     def talk(self):
-        print(f'I am a {self.type_of_enemy}. Be Prepared to flight' )
+        print(f'I am a {self.__type_of_enemy}. Be Prepared to flight' )
 
     def walk_forward(self): # buoc ve phia truoc
-        print(f'{self.type_of_enemy} move closer to you')
+        print(f'{self.__type_of_enemy} move closer to you')
 
     def attach(self):
-        print(f'{self.type_of_enemy} attach for {self.attach_damage} dameage')
+        print(f'{self.__type_of_enemy} attach for {self.attach_damage} dameage')
+
+    # vi khai bao la private ne khong the get duoc thuoc tinh prviate tu main
+    # vi vay can khai bao geteer
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+
 
--- pythonProject/Main.py
+++ pythonProject/Main.py
@@ -6,6 +6,4 @@ zombie = Enemy('Zombie', 1 , 10)
 big_zomebie = Enemy('Big Zombie', 10.,100)
 
 # zombie.type_of_enemy = 'Zombie'
-
-print(zombie.attach())
-print(big_zomebie.attach())
+print(zombie.get_type_of_enemy())
