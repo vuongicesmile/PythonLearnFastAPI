e0c2d45e4cd5f3112fc34356f7a6fd75b9c0dac2
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 17:02:23 +0700

    fastAPi - get endpoint

---
+++ fastAPI/books.py
@@ -0,0 +1,20 @@
+from fastapi import *
+
+# this allows uvicorn to identify that hey
+# creating a new app of FastAPI
+# uvicorn is the web server we use to start a FASTAPI application
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/")
+async  def first_api():
+    return {"message": "Hello Vuong"}
