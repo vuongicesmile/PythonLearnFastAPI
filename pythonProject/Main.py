1c77a60aff70f3230e51b0494b7b85f8b9331a5b
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-02 11:31:53 +0700

    polymorphism- tinh da hinh- add function base can use all cho class ke thua

--- pythonProject/Main.py
+++ pythonProject/Main.py
@@ -3,13 +3,20 @@ from Zombie import *
 from Ogre import *
 
 
+def battle(e: Enemy):
+    e.talk()
+    e.attach()
+
+
 zombie = Enemy('Zombie', 1 , 10)
 
 big_zomebie = Enemy('Big Zombie', 10.,100)
 
 zombile1 = Zombie('zombie', 10,1)
 
-ogre = Ogre(20,1)
-# zombie.type_of_enemy = 'Zombie'
-print(zombile1.talk())
-print(ogre.talk())
+# ogre = Ogre(20,1)
+# # zombie.type_of_enemy = 'Zombie'
+# print(zombile1.talk())
+# print(ogre.talk())
+
+battle(zombie)
