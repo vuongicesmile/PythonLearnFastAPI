13f66730abc9b13bff13d3cbe53b7769cee53e0b
Nguyen Quoc Vuong <vuong.quoc@frogasia.com>
2025-03-09 17:13:54 +0700

    New DB and User Table

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,19 @@
 # creating this model for our database.py file
 from database import Base
-from sqlalchemy import  Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -10,3 +23,5 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    # thêm Forign_key
+    owner_id = Column(Integer, ForeignKey("users.id"))
